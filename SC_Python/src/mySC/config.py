1820f2198afc1649715af21c6b635a5efbc10237
arneBolsenkoetter <161606123+arneBolsenkoetter@users.noreply.github.com>
2025-09-29 22:15:41 +0200

    This commit now should include the updated sc_python/src/mySC/config.py

--- SC_Python/src/mySC/config.py
+++ SC_Python/src/mySC/config.py
@@ -4,9 +4,9 @@ from pathlib import Path
 config_path = Path(__file__).resolve()  # config needs to be located like: /path/to/PROJECT_ROOT/PY_DIR/own_code/config.py
 
 PROJECT_ROOT = config_path.parents[3]
-PY_DIR = PROJECT_ROOT / Path('SC_Python')
-DATA_DIR = PROJECT_ROOT / Path('SC_Data')
-LATEX_DIR = PROJECT_ROOT / Path('SC_Latex')
+PY_DIR = PROJECT_ROOT / Path('sc_python')
+DATA_DIR = PROJECT_ROOT / Path('sc_data')
+LATEX_DIR = PROJECT_ROOT / Path('sc_latex')
 
 figrect_norm=(3.375,0.75*3.375) # numbers in inches
 
